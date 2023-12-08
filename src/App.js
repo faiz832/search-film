afdb8d00662f237dcfeb6c9389bc47c7fc586473
faiz832 <faqodefazz@gmail.com>
2023-12-08 10:15:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
 
-function App() {
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="wrapper" key={i}>
+          <img className="img" alt="img" src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+          <div className="sub-title">
+            <div className="title">{movie.title}</div>
+            <div className="sub-wrapper">
+              <div className="date">{movie.release_date}</div>
+              <div className="rate">⭐{movie.vote_average}</div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="nav nav-container">
+          <a href=".nav" className="logo">
+            Film
+          </a>
+          <div className="search-container">
+            <input type="text" className="search" placeholder="Search" onChange={({ target }) => search(target.value)} />
+            <a href=".nav" className="login">
+              Login
+            </a>
+          </div>
+        </div>
+        <div className="wallpaper">
+          <h1 className="wallpaper-title">
+            EXPLORE <span>THE AMAZING MOVIES</span>
+          </h1>
+        </div>
+        <h1 className="judul">Featured Movies</h1>
+        <div className="film-container container">
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
